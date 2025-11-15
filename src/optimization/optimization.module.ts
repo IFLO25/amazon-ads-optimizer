ba1b43ec688d13413a97a58bd21631edd09ef6a0
IFLO25 <weigelt-christian@web.de>
2025-11-15 13:56:09 +0100

    Create optimization.module.ts

---
+++ src/optimization/optimization.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OptimizationController } from './optimization.controller';
+import { OptimizationService } from './optimization.service';
+
+@Module({
+  controllers: [OptimizationController],
+  providers: [OptimizationService],
+  exports: [OptimizationService],
+})
+export class OptimizationModule {}
